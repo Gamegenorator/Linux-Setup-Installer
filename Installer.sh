6d01f0b7562ad0c7209e8f750c1e63fa5d79de31
Gamegenorator <pierce.sonnenberg5@gmail.com>
2020-02-01 16:55:43 -0800

    Fixed htop, evoulution, & Bitwarden Installation. Added System Updates to script, as well as a cleanup at the end. Removed NodeJS support for Debian (For Now). Added echos through the process to explain what is being done.

--- Installer.sh
+++ Installer.sh
@@ -1,46 +1,59 @@
 #!/bin/sh
 echo "This is untested for the time being and is not guaranteed to work, with that said,"
-echo "Beginning Process"
+echo "Beginning Process..."
+# Update System
+echo "Installing System Updates"
+sudo apt update
+sudo apt-get dist-upgrade
+sudo apt install
 # Install git
+echo "Installing git"
 sudo apt install git
 # Install neofetch
+echo "Installing Neofetch"
 sudo apt install neofetch
 # Install Htop
-sudo apt install Htop
+echo "Installing Htop"
+sudo apt install htop
 # Install Evolution Email Client
-sudo apt install Evolution
+echo "Installing Evolution Email"
+sudo apt install evolution
 # Install Bitwarden Password Manager
-sudo apt install Bitwarden
+echo "Installing Bitwarden Password Manager"
+sudo apt install bitwarden
 # Install KeePassXC Password Manager
+echo "Installing KeePassXC"
 sudo apt install keepassxc
 # Install Brave Browser
+echo "Installing the Brave Browser"
 sudo apt install apt-transport-https curl
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser
 # Install newest version of NodeJS 12 LTS
-echo -n "Is this distro closer to Debian or Ubuntu? (1 = Debian/2 = Ubuntu)?"
-read answer
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-# Using Debian, as root
-curl -sL https://deb.nodesource.com/setup_12.x | bash -
-sudo apt-get install -y nodejs
-sudo apt-get install gcc g++ make
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
-else
-# Using Ubuntu
+echo "Installing NodeJS 12"
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install gcc g++ make
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
-fi
 node --version
 npm --version
+# Install Yarm
+echo "Installing yarn"
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
 # Install Electron (Requires Nodejs)
-npm install electron --save-dev
+echo "Installing Electronjs"
+npm install electron -g
 # Install ReactNative (Requires Nodejs)
+echo "Installing React-Native"
 npm install -g expo-cli
 # Install Redux (Requires Nodejs)
+
+# Cleanup
+echo "Cleanning Up..."
+sudo apt autoremove
+
+# Done
+echo "Done!"
